f81a355b198849cb0090182528b73fcbbf9cb8de
JeissonHrdz <jeissonhrdz186@gmail.com>
2025-03-11 10:45:21 -0500

    ADD: JWT session and implement cookie parser

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
-export const { PORT = 3000, SALT_ROUNDS = 10 } = process.env
+export const {
+  PORT = 3000,
+  SALT_ROUNDS = 10
+  // AQUI VA LA SK
+} = process.env
--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 import express from 'express'
-
-import { PORT } from './config.js'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
+import { PORT, SECRETE_JWT_KEY } from './config.js'
 import { UserRepository } from './user-repository.js'
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.json())
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
-  res.render('index')
+  const token = req.cookies.access_token
+  if (!token) return res.render('index')
+  try {
+    const data = jwt.verify(token, SECRETE_JWT_KEY)
+    res.render('index', data)
+  } catch (error) {
+    res.render('index')
+  }
 })
 
 app.post('/login', async (req, res) => {
   const { username, password } = req.body // Cuerpo de la peticion
   try {
     const user = await UserRepository.login({ username, password })
-    res.send(user)
+    const token = jwt.sign({ id: user._id, username: user.username }, SECRETE_JWT_KEY, {
+      expiresIn: '1h'
+    })
+    res
+      .cookie('access_token', token, {
+        httpOnly: true, // la coockie solo se puede leer en el servidor
+        secure: process.env.NODE_ENV === 'production', // solo se puede acceder en https
+        sameSite: 'strict', // solo se puede acceder desde el mismo dominio
+        maxAge: 60 * 60 * 1000 // 1 hora
+      })
+      .send(user)
   } catch (error) {
     res.status(401).json({ error: error.message })
   }
@@ -37,8 +56,15 @@ app.post('/logout', (req, res) => {
 
 })
 
-app.post('/protected', (req, res) => {
-
+app.get('/protected', (req, res) => {
+  const token = req.cookies.access_token
+  if (!token) return res.status(403).send('unauthorized')
+  try {
+    const data = jwt.verify(token, SECRETE_JWT_KEY)
+    res.render('protected', data)
+  } catch (error) {
+    res.status(401).send('unauthorized')
+  }
 })
 
 app.listen(PORT, () => {
